45b35596db2341aa3ba3c94dd7d4f465df20c0ad
Romain <roch.romain@live.fr>
2017-02-20 16:40:29 +0100

    On a commencé Jacques

--- Projet_Compil/lang.ml
+++ Projet_Compil/lang.ml
@@ -1,44 +1,43 @@
 (* Definition of source language data structures *)
 
 (* variable names *)
-type vname = string
+type vname = string;;
 
 (* function names *)
-type fname = string
+type fname = string;;
 
-type binding = Local | Global
+type binding = Local | Global;;
 
 (* binary arithmetic operators *)
-type barith = BAadd | BAsub | BAmul | BAdiv | BAmod
+type barith = BAadd | BAsub | BAmul | BAdiv | BAmod;;
 
 (* binary logic operators *)
-type blogic = BLand | BLor
+type blogic = BLand | BLor;;
 
 (* binary comparison operators *)
-type bcompar = BCeq | BCge | BCgt | BCle | BClt | BCne
+type bcompar = BCeq | BCge | BCgt | BCle | BClt | BCne;;
 
 (* binary operators, combining all of the above *)
 type binop =
     BArith of barith
   | BCompar of bcompar
-  | BLogic of blogic
-
+  | BLogic of blogic;;
 
 type value =
     BoolV of bool
   | IntV of int
-  | VoidV
+  | VoidV;;
 
-type var = Var of binding * vname
+type var = Var of binding * vname;;
 
-(* expresssions. 
+(* expresssions.
    The type parameter 'a is instantiated during type inference *)
-type 'a expr = 
+type 'a expr =
     Const of 'a * value                        (* constant *)
   | VarE of 'a * var                (* variable *)
   | BinOp of 'a * binop * ('a expr) * ('a expr)   (* binary operation *)
   | IfThenElse of 'a * ('a expr) * ('a expr) * ('a expr) (* if - then - else *)
-  | CallE of 'a * fname * ('a expr list)
+  | CallE of 'a * fname * ('a expr list);;
 
 (* auxiliary function; extracts the type component of an expression *)
 let tp_of_expr = function
@@ -46,45 +45,45 @@ let tp_of_expr = function
   | VarE (t, _) -> t
   | BinOp (t, _, _, _) -> t
   | IfThenElse (t, _, _, _) -> t
-  | CallE (t, _, _) -> t
+  | CallE (t, _, _) -> t;;
 
-(* expresssions. 
+(* expresssions.
    The type parameter 'a is as for expresssions *)
 type 'a stmt =
     Skip
   | Assign of 'a * var * ('a expr)
   | Seq of ('a stmt) * ('a stmt)
-  | Cond of ('a expr) * ('a stmt) * ('a stmt) 
+  | Cond of ('a expr) * ('a stmt) * ('a stmt)
   | While of ('a expr) * ('a stmt)
   | CallC of fname * ('a expr list)
-  | Return of ('a expr)
+  | Return of ('a expr);;
 
 
 (* Types *)
-type tp = BoolT | IntT | VoidT
+type tp = BoolT | IntT | VoidT;;
 
 let numeric_tp = function
     IntT -> true
-  | t -> false
+  | t -> false;;
 
 (* variable / parameter declaration *)
 type vardecl =
-    Vardecl of tp * vname
+    Vardecl of tp * vname;;
 
-let tp_of_vardecl (Vardecl (t, _)) = t
-let name_of_vardecl (Vardecl (_, vn)) = vn
+let tp_of_vardecl (Vardecl (t, _)) = t;;
+
+let name_of_vardecl (Vardecl (_, vn)) = vn;;
 
 (* function declaration: return type; parameter declarations *)
-type fundecl = 
-    Fundecl of tp * fname * (vardecl list)
+type fundecl =
+    Fundecl of tp * fname * (vardecl list);;
 
-let params_of_fundecl (Fundecl (t, fn, pds)) = pds
+let params_of_fundecl (Fundecl (t, fn, pds)) = pds;;
 
 (* function definition: function declaration; local var decls; function body *)
 type 'a fundefn =
-    Fundefn of fundecl * (vardecl list) * ('a stmt)
+    Fundefn of fundecl * (vardecl list) * ('a stmt);;
 
 (* program: global variable declarations; function definitions *)
-type 'a prog = 
-    Prog of (vardecl list) * ('a fundefn list)
-
+type 'a prog =
+    Prog of (vardecl list) * ('a fundefn list);;
--- Projet_Compil/typing.ml
+++ Projet_Compil/typing.ml
@@ -5,15 +5,45 @@ open Analyses
 
 (* Environments *)
 
-type environment = 
-    {localvar: (vname * tp) list; 
-     globalvar: (vname * tp) list; 
+type environment =
+    {localvar: (vname * tp) list;
+     globalvar: (vname * tp) list;
      returntp: tp;
-     funbind: fundecl list}
+     funbind: fundecl list};;
 
 
 (* TODO: put your definitions here *)
 let tp_prog (Prog (gvds, fdfs)) =
   Prog([],
        [Fundefn (Fundecl (BoolT, "even", [Vardecl (IntT, "n")]), [], Skip)])
+;;
+
+(* Const of 'a * value                        (* constant *)
+| VarE of 'a * var                (* variable *)
+| BinOp of 'a * binop * ('a expr) * ('a expr)   (* binary operation *)
+| IfThenElse of 'a * ('a expr) * ('a expr) * ('a expr) (* if - then - else *)
+| CallE of 'a * fname * ('a expr list) *)
+
+exception PasDeVariable;;
+
+exception MauvaisTypage;;
+
+let rec findVar name = function
+    ((s,t)::l) -> if s = name then t else findVar name l
+    |[] -> raise PasDeVariable;;
+
+let rec tp_expr env = function
+    Const(_,n) -> (match n with
+                        (BoolV b) -> Const(BoolT b,BoolV b)
+                        |(IntV i) -> Const(IntT i,IntV i)
+                        |(VoidV) -> Const(VoidT,VoidV))
+    |VarE(_,Var(bind,name)) -> (try VarE(findVar name env.localvar,Var(bind,name)) with
+                                                        PasDeVariable -> raise MauvaisTypage)
+    |BinOp(_,opBin,exp1,exp2) -> if tp_expr env exp1 = tp_expr env exp2 && tp_expr env exp1 =
+
+      | BCompar of bcompar
+      | BLogic of blogic)
+
+
+
 ;;
