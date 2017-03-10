3094b16fd95d49e416aaa3a6c8f9ea05e366c59d
TheHaricover <roch.romain@live.fr>
2017-03-10 18:40:03 +0100

    Done ! The project is over until the genExpr function. All the functions are tested via unit tests.

--- Projet_Compil/gen.ml
+++ Projet_Compil/gen.ml
@@ -3,6 +3,7 @@
 open Lang
 open Analyses
 open Instrs
+open Typing (* For tests *)
 
 (* ************************************************************ *)
 (* **** Compilation of expressions / statements            **** *)
@@ -21,18 +22,35 @@ let gen_prog (Prog (gvds, fdfs)) =
                       [Loadc (IntT, IntV 0); ReturnI IntT])]);;
 
 
-exception ElementNonTrouve;;
+exception ElementNotFound;;
 
+(* this function find the position of the element n and return it *)
 let rec position n = function
     (a::l) -> if a = n
               then 0
               else 1 + (position n l)
-    |[] -> raise ElementNonTrouve;;
+    |[] -> raise ElementNotFound;;
 
-
+(* genExpr generate the "java bytecode" expression *)
 let rec genExpr varList = function
     Const (t,v) -> [Loadc (t,v)]
-    | VarE (t,v) -> [Loadv (t,position v varList)]
+    | VarE (t,v) -> [Loadv (t,position (VarE (t,v)) varList)]
     | BinOp (t,binOp,exp1,exp2) -> (genExpr varList exp1)@(genExpr varList exp2)@[Bininst(t,binOp)]
     (*| IfThenElse (t, _, _, _) ->
     | CallE (t, _, _) -> *);;
+
+
+(* Here's the tests for genExpr *)
+(* If you have some hard times to make the tests work, then copy-paste all the "typing.ml" file in this file. *)
+let env = {localvar = [("k", IntT);
+                       ("n", IntT)];
+           globalvar = [];
+           returntp = VoidT;
+           funbind = [Fundecl(IntT,"f",[Vardecl (IntT,"y");Vardecl (BoolT,"jacques")])]};;
+
+
+let expr3 = BinOp (0, BCompar BCeq, VarE (0, Var (Local, "n")),
+                      BinOp (0, BArith BAadd, VarE (0, Var (Local, "k")),
+                                             Const (0, IntV 1)));;
+
+genExpr ([(VarE (IntT, (Var (Local, "n"))));(VarE (IntT, (Var (Local, "k"))))]) (tp_expr env expr3);;
--- Projet_Compil/typing.ml
+++ Projet_Compil/typing.ml
@@ -4,15 +4,9 @@ open Lang
 open Analyses
 
 
-
-
-
-
-
-
-
-
-
+(* I copy-paste this function here because if we just "open Lang", the "make"
+	command tells us that Lang is unbound or so. By putting this function that
+	comes from "lang.ml", the "make" command is happy and let us compile.  *)
 
 (* auxiliary function; extracts the type component of an expression *)
 let tp_of_expr = function
@@ -21,24 +15,6 @@ let tp_of_expr = function
   | BinOp (t, _, _, _) -> t
   | IfThenElse (t, _, _, _) -> t
   | CallE (t, _, _) -> t;;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 (* Environments *)
@@ -61,39 +37,44 @@ let tp_prog (Prog (gvds, fdfs)) =
 | IfThenElse of 'a * ('a expr) * ('a expr) * ('a expr) (* if - then - else *)
 | CallE of 'a * fname * ('a expr list) *)
 
-exception PasDeVariable;;
+exception NoVariable;;
+exception NoFunction;;
+exception BadTyping;;
 
-exception MauvaisTypage;;
 
-
+(* This function find the type of a variable given on parameter *)
 let rec findTypeOfVar name = function
-    ((s,t)::l) -> if s = name then t else findTypeOfVar name l
-    |[] -> raise PasDeVariable;;
+    ((s,t)::l) -> if s = name
+				  then t
+				  else findTypeOfVar name l
+    |[] -> raise NoVariable;;
 
+(* This function filter the different type of a binop *)
 let opBinAux = function
     (BArith arithOp,exp1tp,exp2tp) -> let typeExpr1 = (tp_of_expr (exp1tp))
-                                      in let typeExpr2  = (tp_of_expr (exp2tp))
-                                         in
-                                         if typeExpr1 = typeExpr2
-                                         && (typeExpr1 = IntT)
-                                         then IntT
-                                         else raise MauvaisTypage
+									  and typeExpr2  = (tp_of_expr (exp2tp))
+									  in
+									  	if typeExpr1 = typeExpr2
+										&& (typeExpr1 = IntT)
+										then IntT
+										else raise BadTyping
 
     |(BCompar compOp,exp1tp,exp2tp) -> let typeExpr1 = (tp_of_expr (exp1tp))
-                                       in let typeExpr2  = (tp_of_expr (exp2tp))
-                                          in
-                                          if typeExpr1 = typeExpr2
-                                          then BoolT
-                                          else raise MauvaisTypage
+									   and typeExpr2  = (tp_of_expr (exp2tp))
+									   in
+									   	if typeExpr1 = typeExpr2
+										then BoolT
+										else raise BadTyping
 
     |(BLogic logicOp,exp1tp,exp2tp) -> let typeExpr1 = (tp_of_expr (exp1tp))
                                        and typeExpr2  = (tp_of_expr (exp2tp))
-                                          in
-                                          if typeExpr1 = typeExpr2
-                                          && (typeExpr1 = BoolT)
-                                          then BoolT
-                                          else raise MauvaisTypage;;
+                                       in
+									   	if typeExpr1 = typeExpr2
+										&& (typeExpr1 = BoolT)
+										then BoolT
+										else raise BadTyping;;
 
+(* ifAux is here to determine if the if is well constructed and typed *)
 let ifAux = function
     (exp1tp,exp2tp,exp3tp) -> let typeExpr1 = (tp_of_expr (exp1tp))
                               and typeExpr2  = (tp_of_expr (exp2tp))
@@ -102,32 +83,32 @@ let ifAux = function
                                 if typeExpr1 = BoolT
                                 && typeExpr2 = typeExpr3
                                 then typeExpr2
-                                else raise MauvaisTypage;;
+                                else raise BadTyping;;
 
-
-exception FonctionNonExistante;;
-
+(* foncAux returns the Fundecl associated to the function in the funbind of an environnement *)
 let rec foncAux (nomF,env) = function
     ((Fundecl (typeF,fname,varL))::funbindL) -> (if fname = nomF
                                                  then Fundecl (typeF,fname,varL)
                                                  else (foncAux (nomF,env) funbindL))
-    |[] -> raise FonctionNonExistante;;
+    |[] -> raise NoFunction;;
 
+(* tp_expr is the principal function. It type all the 'a expr available *)
 let rec tp_expr env = function
     (Const((_ : int),c)) -> (match c with
-                        (BoolV b) -> Const(BoolT,BoolV b)
-                        |(IntV i) -> Const(IntT,IntV i)
-                        |(VoidV) -> raise MauvaisTypage)
+									(BoolV b) -> Const(BoolT,BoolV b)
+									|(IntV i) -> Const(IntT,IntV i)
+									|(VoidV) -> raise BadTyping)
 
     |(VarE(_,Var(bind,name))) -> (try VarE(findTypeOfVar name (match bind with
                                                                         Local -> env.localvar
                                                                         |Global -> env.globalvar),Var(bind,name))
                                   with
-                                    PasDeVariable -> raise MauvaisTypage)
+                                    NoVariable -> raise BadTyping)
 
-    |(BinOp(_ ,opBin,exp1,exp2)) -> let exp1tp = (tp_expr env exp1) and exp2tp = (tp_expr env exp2)
-                                        in
-                                            (BinOp(opBinAux(opBin,exp1tp,exp2tp),opBin,exp1tp,exp2tp))
+    |(BinOp(_ ,opBin,exp1,exp2)) -> let exp1tp = (tp_expr env exp1)
+									and exp2tp = (tp_expr env exp2)
+									in
+										(BinOp(opBinAux(opBin,exp1tp,exp2tp),opBin,exp1tp,exp2tp))
 
     |(IfThenElse (_,exp1,exp2,exp3)) -> (let exp1tp = (tp_expr env exp1)
                                          and exp2tp = (tp_expr env exp2)
@@ -137,15 +118,18 @@ let rec tp_expr env = function
 
     |(CallE (_,nomF,declVar)) -> let Fundecl (typeF,nameF,varLF) = (foncAux (nomF,env) (env.funbind))
                                  in let rec compTypeAux = function
-                                        ((e::l1),((Vardecl (tp,_))::l2)) -> let exp = (tp_expr env e)
+								 							((e::l1),((Vardecl (tp,_))::l2)) -> let exp = (tp_expr env e)
                                                                             in let tpExp = (tp_of_expr exp)
                                                                                in (if tpExp = tp
                                                                                   then exp::compTypeAux(l1,l2)
-                                                                                  else raise MauvaisTypage)
-                                        |([],[]) -> []
-                                        |([],_) -> raise MauvaisTypage
-                                        |(_,[]) -> raise MauvaisTypage
-                                 in CallE (typeF,nomF,(compTypeAux (declVar,varLF)));;
+                                                                                  else raise BadTyping)
+															|([],[]) -> []
+															|([],_) -> raise BadTyping
+															|(_,[]) -> raise BadTyping
+                                 	in CallE (typeF,nomF,(compTypeAux (declVar,varLF)));;
+
+
+(* Here are the tests ! *)
 
 (* let env = {localvar = [("k", IntT);
                        ("n", IntT)];
@@ -168,6 +152,21 @@ let expr4 = VarE (0, Var(Local,"k"));;
 let expr5 = IfThenElse (0,expr3,expr2,expr4);;
 
 let expr6 = CallE (0,"f",[Const (0,IntV 2);Const (0,BoolV true)]);;
+
+tp_expr env (expr1);;
+tp_of_expr (tp_expr env (expr1));;
+
+tp_expr env (expr2);;
+tp_of_expr (tp_expr env (expr2));;
+
+tp_expr env (expr3);;
+tp_of_expr (tp_expr env (expr3));;
+
+tp_expr env (expr4);;
+tp_of_expr (tp_expr env (expr4));;
+
+tp_expr env (expr5);;
+tp_of_expr (tp_expr env (expr5));;
 
 tp_expr env (expr6);;
 tp_of_expr (tp_expr env (expr6));; *)
